6dd72ae22bc15da8bbed6c93241f42b086e75e54
JANGYANG <radiata03@gmail.com>
2019-12-08 22:20:36 +0900

    debug false

--- apple/settings.py
+++ apple/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e0h#nadkggio#s8of9b!zxwor08w4$!)t2s+o1u(*ooxi@e2cl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
